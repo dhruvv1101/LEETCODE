4d4c2089527dcc68fa0110c321cf42fcceba5173
dhruv831v <dhruv83170@gmail.com>
2025-05-28 19:05:47 +0530

    05-28-2025 1091-shortest-path-in-binary-matrix Medium .cpp 39 ms (29.76%) 23.8 MB (77.27%)

--- LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ LeetCode/Medium/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,40 +1,44 @@
+typedef pair<int, pair<int, int>> p;
+
 class Solution {
 public:
-    bool issafe(int xn ,int yn,int n,int m){
-        return xn>=0&&xn<n&&yn>=0&&yn<m;
+    bool issafe(int xn, int yn, int n, int m) {
+        return xn >= 0 && xn < n && yn >= 0 && yn < m;
     }
+
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        if(n==0||m==0||grid[0][0]!=0){
+        int n = grid.size();
+        int m = grid[0].size();
+
+        if (n == 0 || m == 0 || grid[0][0] != 0 || grid[n - 1][m - 1] != 0)
             return -1;
-        }
-        vector<vector<int>>dir={{1,1},{1,0},{0,1},{-1,1},{1,-1},{-1,-1},{-1,0},{0,-1}};// rd r d ld ru lu l u 
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        grid[0][0]=1;
-        int level=0;
-        while(!q.empty()){
-            int N=q.size();
-            while(N--){
-                auto cur=q.front();
-                q.pop();
-                int x=cur.first;
-                int y=cur.second;
-                if(x==n-1&&y==m-1){
-                    return level+1;
-                }
-                for(auto d:dir){
-                    int xn=x+d[0];
-                    int yn=y+d[1];
-                    if(issafe(xn,yn,n,m)&&grid[xn][yn]==0){
-                        grid[xn][yn]=level+1;
-                        q.push({xn,yn});
-                    }
+
+        vector<vector<int>> dir = {{1,1},{1,0},{0,1},{-1,1},{1,-1},{-1,-1},{-1,0},{0,-1}};
+        vector<vector<int>> result(n, vector<int>(m, INT_MAX));
+        priority_queue<p, vector<p>, greater<p>> pq;
+
+        pq.push({1, {0, 0}});
+        result[0][0] = 1;
+
+        while (!pq.empty()) {
+            auto [dist, node] = pq.top();
+            pq.pop();
+            int x = node.first;
+            int y = node.second;
+
+            if (x == n - 1 && y == m - 1) return dist;
+
+            for (auto& dxy : dir) {
+                int xn = x + dxy[0];
+                int yn = y + dxy[1];
+
+                if (issafe(xn, yn, n, m) && grid[xn][yn] == 0 && dist + 1 < result[xn][yn]) {
+                    result[xn][yn] = dist + 1;
+                    pq.push({result[xn][yn], {xn, yn}});
                 }
             }
-            level++;
         }
+
         return -1;
     }
 };
