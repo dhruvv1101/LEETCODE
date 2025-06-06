f65beb2b8f924c71317641bc2c7a379cdd2b228f
dhruv831v <dhruv83170@gmail.com>
2025-06-06 17:57:27 +0530

    06-06-2025 0200-number-of-islands Medium .cpp 23 ms (84.18%) 17.6 MB (45.19%)

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,33 +1,66 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty() || grid[0].empty()) {
-            return 0;
+    vector<int> parent, rank;
+
+    int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);  // Path compression
         }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
+        return parent[x];
+    }
+
+    void unions(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py) return;
+
+        if (rank[px] < rank[py]) {
+            parent[px] = py;
+        } else if (rank[px] > rank[py]) {
+            parent[py] = px;
+        } else {
+            parent[py] = px;
+            rank[px]++;
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        parent.resize(n * m);
+        rank.resize(n * m, 0);
+        int count = 0;
+
+        // Initialize DSU only for land cells
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+                    int id = i * m + j;
+                    parent[id] = id;
+                    count++;
                 }
             }
         }
-        
-        return numIslands;
-    }
-    
-private:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
-            return;
+
+        // Directions: up, down, left, right
+        vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    int id1 = i * m + j;
+                    for (auto [dx, dy] : dirs) {
+                        int ni = i + dx, nj = j + dy;
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < m && grid[ni][nj] == '1') {
+                            int id2 = ni * m + nj;
+                            if (find(id1) != find(id2)) {
+                                unions(id1, id2);
+                                count--;
+                            }
+                        }
+                    }
+                }
+            }
         }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+
+        return count;
     }
 };
