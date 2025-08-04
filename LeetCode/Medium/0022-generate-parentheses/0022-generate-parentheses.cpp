0e1f6d276663db51fd2609dcea6aaabab45aa77a
dhruv831v <dhruv83170@gmail.com>
2025-08-04 18:21:52 +0530

    08-04-2025 0022-generate-parentheses Medium .cpp 0 ms (100%) 13.2 MB (78.59%)

--- LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.cpp
+++ LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     vector<string>res;
-    bool isval(string s){
-        int o=0;
-        for(auto it:s){
-            if(it=='('){
-                o++;
-            }else{
-                o--;
-                if(o<0){
-                    return 0;
-                }
-            }
-        }
-        return o==0;
-    }
-    void solve(int n,string &s){
+    
+    void solve(int n,string &s,int o,int c){
         if(s.length()==2*n){
-            if(isval(s)){
                 res.push_back(s);
-            }
             return;
         }
+        if(o<n){
         s.push_back('(');
-        solve(n,s);
-        s.pop_back();
+        solve(n,s,o+1,c);
+        s.pop_back();}
+        if(c<o){
         s.push_back(')');
-        solve(n,s);
-        s.pop_back();
+        solve(n,s,o,c+1);
+        s.pop_back();}
     }
     vector<string> generateParenthesis(int n) {
         string cur="";
-        solve(n,cur);
+        int o=0,c=0;
+        solve(n,cur,o,c);
         return res;
     }
 };
