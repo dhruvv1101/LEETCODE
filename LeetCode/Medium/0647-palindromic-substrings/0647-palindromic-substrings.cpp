fd8da5325d555935786c6c5f0a51c5668bc4f139
dhruv831v <dhruv83170@gmail.com>
2025-08-10 19:18:33 +0530

    08-10-2025 0647-palindromic-substrings Medium .cpp 31 ms (31.89%) 10.9 MB (57.3%)

---
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n=s.length();
+        vector<vector<bool>>t(n,vector<bool>(n,0));
+        int cnt=0;
+        for(int l=1;l<=n;l++){
+            for(int i=0;i+l-1<n;i++){
+                int j=i+l-1;
+                if(i==j){
+                    t[i][i]=1;
+                }else if(i+1==j){
+                    t[i][j]=(s[i]==s[j])?1:0;
+                }else{
+                    t[i][j]=(s[i]==s[j]&&t[i+1][j-1]);
+                }
+                if(t[i][j]){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
