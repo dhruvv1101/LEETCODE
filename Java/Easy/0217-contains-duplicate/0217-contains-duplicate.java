5d69172f403f007f754c84ca49fe5c94a4b002d1
Dhruv Verma <dhruv.verma@i.curefit.com>
2026-08-24 17:43:01 +0530

    08-24-2026 .java Array Easy 20 ms (38.99%)

--- Java/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ Java/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,6 +2,9 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
             Set <Integer> all=new HashSet<>();
             for(int n :nums){
+                if(all.contains(n)){
+                    return true;
+                }
                 all.add(n);
             }
             return all.size()!=nums.length;
